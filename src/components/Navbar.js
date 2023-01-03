a6913df3a82e48406a82a56b4bb353cf3a71aaeb
Pujan Soni <65944163+pujan2308@users.noreply.github.com>
2023-01-04 02:05:20 +0530

    Routing logo error solved 3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,6 @@ function NavBar() {
       <Container>
         <Navbar.Brand href="/" className="d-flex">
           <img src={logo} className="img-fluid logo" alt="brand"/>
-          <Router basename="/pujan"></Router>
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
